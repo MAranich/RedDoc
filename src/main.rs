5b5778eafadb0c09b60dd8d655756c70369aad72
MAranich <marcel.aranich01@estudiant.upf.edu>
2026-04-07 10:50:59 +0200

    Added implementation for action custom command

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,98 @@
 //! # RedDoc
-//! 
-//! RedDoc is a tool for documentation targeted to cybersecurity profesionals. 
-//! 
-//! 
+//!
+//! RedDoc is a tool for documentation targeted to cybersecurity profesionals.
+//!
+//!
 
-use clap::{Command, command};
+use clap::{Arg, ArgMatches, Command, command};
 
-const ABOUT_ACTION_CLAP: &'static str = "Adds a node that represents an action. ";
-const ABOUT_CONSEQUENCE_CLAP: &'static str =
+use crate::node::*;
+
+const ABOUT_ACTION_CLAP: &str = "Adds a node that represents an action. ";
+const ABOUT_CONSEQUENCE_CLAP: &str =
     "Add a node that represents a consequence to a previous action. ";
-const ABOUT_EVENT_CLAP: &'static str = "Add an event, a *consequence* without your cause";
-const ABOUT_COMMAND_CLAP: &'static str = "Execute the provided command and document it. ";
+const ABOUT_EVENT_CLAP: &str = "Add an event, a *consequence* without your cause";
+const ABOUT_COMMAND_CLAP: &str = "Execute the provided command and document it. ";
 
-pub mod node;
+/// Sub command action
+const SUB_ACTION: &str = "action";
+/// Sub command Consequence
+const SUB_CONSEQUENCE: &str = "consequence";
+/// Sub command event
+const SUB_EVENT: &str = "event";
+/// Sub command *command*
+const SUB_COMMAND: &str = "command";
+
+// Sub commands fot action:
+const ACTION_CUSTOM: &str = "custom";
+const ACTION_CUSTOM_ABOUT: &str =
+    "Introduce any text you want to be saved between quotes. \"Hello world!\"
+
+This option can be used to store: 
+ - Comments
+ - Scenatios not accounted in the program. ";
+
 pub mod information;
+pub mod node;
+
+// ****************************
+
+const DEBUG_MODE: bool = true;
 
 fn main() {
     let matches: clap::ArgMatches = command!()
         .subcommand(
-            Command::new("action")
+            Command::new(SUB_ACTION)
                 .about(ABOUT_ACTION_CLAP)
                 .alias("act")
-                .alias("a"),
+                .alias("a")
+                .subcommand(
+                    Command::new(ACTION_CUSTOM)
+                        .arg(Arg::new("content"))
+                        .about(ACTION_CUSTOM_ABOUT),
+                ),
         )
         .subcommand(
-            Command::new("consequence")
+            Command::new(SUB_CONSEQUENCE)
                 .about(ABOUT_CONSEQUENCE_CLAP)
                 .alias("cons"),
         )
-        .subcommand(Command::new("event").about(ABOUT_EVENT_CLAP).alias("ev"))
-        .subcommand(Command::new("command").about(ABOUT_COMMAND_CLAP).alias("c"))
+        .subcommand(Command::new(SUB_EVENT).about(ABOUT_EVENT_CLAP).alias("ev"))
+        .subcommand(
+            Command::new(SUB_COMMAND)
+                .about(ABOUT_COMMAND_CLAP)
+                .alias("c"),
+        )
         .get_matches();
+
+    let _ = match matches.subcommand() {
+        Some((SUB_ACTION, sub_match)) => process_action(sub_match),
+        Some(_) => todo!("Unrecognized subcommand provided"),
+        None => todo!("No subcommand provided. "),
+    };
+}
+
+fn process_action(sub_match: &ArgMatches) -> Result<(), ()> {
+    if DEBUG_MODE {
+        println!("Action detected! Processing...");
+    }
+
+    let new_node: Node = match sub_match.subcommand() {
+        Some((ACTION_CUSTOM, raw_content)) => {
+            let content: Option<&String> = raw_content.get_one::<String>("content");
+
+            let content: &str = match content {
+                Some(string) => string.as_str(),
+                None => "",
+            };
+
+            Node::new(Category::Action(Action::Custom(String::from(content))))
+        }
+        Some(_) => todo!("Unrecognized action subcommand provided"),
+        None => todo!("No action subcommand provided. "),
+    };
+
+    println!("New node: \n{:?}", new_node); 
+
+    return Ok(());
 }
--- src/node.rs
+++ src/node.rs
@@ -1,73 +1,75 @@
-//! 
-//! A module dedicated to the structure [Node], wich is the fundamental building block 
-//! for storing the information. 
-//! 
+//!
+//! A module dedicated to the structure [Node], wich is the fundamental building block
+//! for storing the information.
+//!
 
-
+use crate::information::InfoRef;
 use chrono::prelude::*;
-use crate::information::InfoRef; 
 
 /// Basic structure that stores the relevant information
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub struct Node {
-    time_stamp: DateTime<Utc>, 
-    category: Category, 
+    time_stamp: DateTime<Utc>,
+    category: Category,
 }
 
-/// The different classes of events that can happen: 
+/// The different classes of events that can happen:
 ///  - [Category::Action]: Codifies actions taken by the user
-///  - [Category::Consequence]: Codifies the consequences of actions. 
-///  - [Category::Event]: Codifies any other fact that cannot be direcly attributed to an action. 
+///  - [Category::Consequence]: Codifies the consequences of actions.
+///  - [Category::Event]: Codifies any other fact that cannot be direcly attributed to an action.
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub enum Category {
-    Action(Action), 
-    Consequence(Consequence), 
-    Event(Event), 
+    Action(Action),
+    Consequence(Consequence),
+    Event(Event),
 }
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub enum Action {
     /// A custom action, the user may store any string
-    Custom(String), 
+    Custom(String),
     /// The execution of a command (the output is sored in another node)
-    Command(String), 
-    KnownCommnad(KnownCommnad), 
+    Command(String),
+    KnownCommnad(KnownCommnad),
     // /// Execution of a script, string is for the code
     // Script(String)
-
 }
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub enum KnownCommnad {
-    A, B, C
+    A,
+    B,
+    C,
 } // TODO
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub enum Consequence {
-    Custom(String), 
+    Custom(String),
     /// Output of a command
-    Command(String), 
-    NewInformation(InfoRef), 
-    /// The Blue team (Defenders) have discovered the activity. 
-    Detection, 
+    Command(String),
+    NewInformation(InfoRef),
+    /// The Blue team (Defenders) have discovered the activity.
+    Detection,
     /// No consequence for the previous action
-    None, 
+    None,
     // Unresolved(String)
 }
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub enum Event {
-    Custom(String), 
-    /// The Blue team (Defenders) have discovered the activity. 
-    Detection, 
-
+    Custom(String),
+    /// The Blue team (Defenders) have discovered the activity.
+    Detection,
 }
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
-pub struct Timeline(Vec<Node>); 
+pub struct Timeline(Vec<Node>);
 
-
-
-
-
-
+impl Node {
+    pub fn new(cat: Category) -> Node {
+        return Node {
+            time_stamp: Utc::now(),
+            category: cat,
+        };
+    }
+}
