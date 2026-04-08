c4bc15e14d90374c5e0ce6d071c0606000aa882a
MAranich <marcel.aranich01@estudiant.upf.edu>
2026-04-08 10:42:16 +0200

    Created implementation for custom event.

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ const SUB_COMMAND: &str = "command";
 /// Sub command configuration
 const SUB_CONFIG: &str = "configuration";
 
-// Sub commands fot action: **********************************************
+// Sub commands for action: **********************************************
 const ACTION_CUSTOM: &str = "custom";
 const ACTION_CUSTOM_ABOUT: &str =
     "Introduce any text you want to be saved between quotes. \"Hello world!\"
@@ -37,7 +37,7 @@ This option can be used to store:
  - Comments
  - Scenatios not accounted in the program. ";
 
-// Sub commands fot consequence: *****************************************
+// Sub commands for consequence: *****************************************
 const CONSEQUENCE_CUSTOM: &str = "custom";
 const CONSEQUENCE_CUSTOM_ABOUT: &str =
     "Introduce any text you want to be saved between quotes. \"Hello world!\"
@@ -45,6 +45,16 @@ const CONSEQUENCE_CUSTOM_ABOUT: &str =
 This option can be used to store: 
  - Comments
  - Scenatios not accounted in the program. ";
+
+// Sub commands for events: **********************************************
+const EVENT_CUSTOM: &str = "custom";
+const EVENT_CUSTOM_ABOUT: &str =
+    "Introduce any text you want to be saved between quotes. \"Hello world!\"
+
+This option can be used to store: 
+ - Comments
+ - Scenatios not accounted in the program. ";
+
 
 pub mod information;
 pub mod node;
@@ -76,13 +86,29 @@ fn main() {
                         .about(CONSEQUENCE_CUSTOM_ABOUT),
                 ),
         )
-        .subcommand(Command::new(SUB_EVENT).about(ABOUT_EVENT_CLAP).alias("ev"))
+        .subcommand(
+            Command::new(SUB_EVENT)
+                .about(ABOUT_EVENT_CLAP)
+                .alias("ev")
+                .alias("e")
+                .subcommand(
+                    Command::new(EVENT_CUSTOM)
+                        .arg(Arg::new("content"))
+                        .about(EVENT_CUSTOM_ABOUT),
+                ),
+        )
         .subcommand(
             Command::new(SUB_COMMAND)
                 .about(ABOUT_COMMAND_CLAP)
+                .alias("comm")
+                .alias("com")
                 .alias("c"),
         )
-        .subcommand(Command::new(SUB_CONFIG).about(ABOUT_CONFIG_CLAP).alias("conf"))
+        .subcommand(
+            Command::new(SUB_CONFIG)
+                .about(ABOUT_CONFIG_CLAP)
+                .alias("conf"),
+        )
         .get_matches();
 
     // //////////
@@ -94,6 +120,7 @@ fn main() {
     let _ = match matches.subcommand() {
         Some((SUB_ACTION, sub_match)) => process_action(sub_match, &mut state),
         Some((SUB_CONSEQUENCE, sub_match)) => process_consequences(sub_match, &mut state),
+        Some((SUB_EVENT, sub_match)) => process_event(sub_match, &mut state),
         Some(_) => todo!("Unrecognized subcommand provided"),
         None => todo!("No subcommand provided. "),
     };
@@ -140,6 +167,28 @@ fn process_consequences(sub_match: &ArgMatches, state: &mut State) {
             Node::new(Category::Consequence(node::Consequence::Custom(
                 String::from(content),
             )))
+        }
+        Some(_) => todo!("Unrecognized action subcommand provided"),
+        None => todo!("No action subcommand provided. "),
+    };
+
+    println!("New node: \n{new_node:?}");
+
+    state.add_node(&new_node);
+}
+
+fn process_event(sub_match: &ArgMatches, state: &mut State) {
+    if DEBUG_MODE {
+        println!("Event detected! Processing...");
+    }
+
+    let new_node: Node = match sub_match.subcommand() {
+        Some((EVENT_CUSTOM, raw_content)) => {
+            let content: Option<&String> = raw_content.get_one::<String>("content");
+
+            let content: &str = content.map_or("", |string| string.as_str());
+
+            Node::new(Category::Event(node::Event::Custom(String::from(content))))
         }
         Some(_) => todo!("Unrecognized action subcommand provided"),
         None => todo!("No action subcommand provided. "),
